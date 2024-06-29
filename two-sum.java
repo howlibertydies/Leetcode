5c86cd07c8c37b38c31140671fab45eaffca3860
Silver <theonesilverest@gmail.com>
2024-06-30 00:31:23 +0530

    Two Sum

---
+++ two-sum.java
@@ -0,0 +1,17 @@
+// Use Hashmap to get O(n) in both space and time complexity
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i <= nums.length-1; i++) {
+            if(map.containsKey(target-nums[i])) {
+                return new int[]{map.get((target-nums[i])),i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{0, 0};
+    }
+}
